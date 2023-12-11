f255b12960415897f107f677cb05e3c903594bc0
vertunoff <anton.s.vertunov@gmail.com>
2023-12-11 18:11:09 +0300

    timer and styles improved

--- webnews/src/components/Clock.js
+++ webnews/src/components/Clock.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useStopwatch, useTime, useTimer } from 'react-timer-hook'
+import TimeWarining from './TimeWarining'
 function Clock(props) {
-    const [noRender, setNoRender] = useState(false)
-    
+
     const time = useTime()
-    // const timeOnSite = useStopwatch()
-    // timeOnSite.start()
-    const [minutesOnSite, setMinutesOnSite] = useState(0)
+    const [minutesOnSite, setMinutesOnSite] = useState(new Date())
     // useEffect(()=> setMinutesOnSite(timeOnSite.seconds), [timeOnSite.minutes])
     const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятницы", "Суббота"]
     const months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Отктября", "Ноября", "Декабря"]
@@ -14,14 +12,24 @@ function Clock(props) {
     const [dayofWeek, setdayofWeek] = useState()
     const [month, setMonth] = useState()
     const [dateNumber, setdateNumber] = useState()
-    useEffect(() => { setDate(new Date()); setdayofWeek(days[date.getDay()]); setdateNumber(date.getDate()); setMonth(months[date.getMonth()]) }, [time.minutes])
+
+
+    useEffect(update, [time.minutes])
+    function update() {
+        setDate(new Date())
+        setdayofWeek(days[date.getDay()])
+        setdateNumber(date.getDate())
+        setMonth(months[date.getMonth()])
+        setMinutesOnSite(Math.round((new Date() - new Date(localStorage.getItem('onsite'))) / 1000 / 60))
+    }
     return (
         <div className='clock'>
             <h1>{dayofWeek}</h1>
             <h1>{`${dateNumber} ${month}`}</h1>
-            <h2>{`${time.hours}:${time.minutes < 10 ? '0'+time.minutes:time.minutes}`}</h2>
+            <h2>{`${time.hours}:${time.minutes < 10 ? '0' + time.minutes : time.minutes}`}</h2>
             <p>Вы на сайте уже</p>
-            <p>{`${1} минут ${minutesOnSite}`}</p>
+            <p>{minutesOnSite ? `${minutesOnSite} минут${Math.floor(minutesOnSite/10)%10 != 1?  minutesOnSite % 10 == 1 ? 'у' : [2, 3, 4].includes(minutesOnSite % 10) ? 'ы' : '':''}` : ''}</p>
+            <TimeWarining time={minutesOnSite}></TimeWarining>
         </div>
     )
 }
---
+++ webnews/src/components/TimeWarining.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TimeWarining(props) {
+  return props.time ==20 ? (
+    <div className='popup-effect'></div>
+  ):''
+}
+
+export default TimeWarining
